0c2ddb595ebc56784dfc76bbf566e410b18e8298
TobiasSA1 <tobiassuarez43@gmail.com>
2023-10-05 21:51:23 +0200

    Primer Parcial

---
+++ Entidades/Numeracion.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Numeracion
+    {
+        //Esistema de Numeracion
+        public enum ESistema
+        {
+            Decimal,
+            Binario
+        }
+
+        //Atributos Numeracion
+        protected static string msgError;
+        protected string valor;
+
+        //Constructor Numeracion
+        protected Numeracion(string valor)
+        {
+            InicializaValor(valor);
+        }
+
+        static Numeracion()
+        {
+            msgError = "Numero Invalido";
+        }
+
+        //Metodo para inicializar el valor
+        private void InicializaValor(string valor)
+        {
+            if (EsNumeracionValida(valor))
+            {
+                this.valor = valor;
+            }
+            else
+            {
+                this.valor = msgError;
+            }
+        }
+
+        //Aplicar de forma obligatoria en clases derivadas
+        abstract internal double ValorNumerico
+        {
+            get;
+        }
+        //Propiedad Valor
+        public string Valor 
+        { 
+            get 
+            {
+                return valor; 
+            }
+        }
+        public abstract Numeracion CambiarSistemaDeNumeracion(ESistema sistema);
+
+        // Método protegido para verificar si la numeración es válida
+        protected bool EsNumeracionValida(string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+
+        public static explicit operator double(Numeracion numeracion)
+        {
+            if (numeracion is null || !numeracion.EsNumeracionValida(numeracion.valor))
+            {
+                return 0.0;
+            }
+
+            if (double.TryParse(numeracion.valor, out double result))
+            {
+                return result;
+            }
+
+            return 0.0;
+        }
+
+        //Sobrecarga para decir que dos numeraciones serán iguales si no son nulas y son del mismo tipo.
+        public static bool operator ==(Numeracion n1, Numeracion n2)
+        {
+            if (ReferenceEquals(n1, null) && ReferenceEquals(n2, null))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(n1, null) || ReferenceEquals(n2, null))
+            {
+                return false;
+            }
+
+            return n1.GetType() == n2.GetType() && n1.Valor == n2.Valor;
+        }
+        //Sobrecarga para decir que dos numeraciones serán iguales si no son nulas y son del mismo tipo.
+
+        public static bool operator !=(Numeracion n1, Numeracion n2)
+        {
+            return !(n1 == n2);
+        }
+
+        //agregaer get hashcode y equals?
+    }
+}
---
+++ Entidades/SistemaBinario.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class SistemaBinario : Numeracion
+    {
+        public SistemaBinario(string valor) : base(valor) { }
+
+        public override Numeracion CambiarSistemaDeNumeracion(ESistema sistema)
+        {
+            if (sistema == ESistema.Binario)
+            {
+                return this;
+            }
+            else if (sistema == ESistema.Decimal)
+            {
+                return BinarioADecimal();
+            }
+            else
+            {
+                // Otros sistemas de numeración podrían ser manejados aquí
+                return this;
+            }
+        }
+
+        public SistemaDecimal BinarioADecimal()
+        {
+            if (!Valor.Contains(msgError))
+            {
+                int valorDecimal = 0;
+                int binario = 1;
+
+                for (int i = Valor.Length - 1; i >= 0; i--)
+                {
+                    if (Valor[i] == '1')
+                    {
+                        valorDecimal += binario;
+                    }
+                    binario *= 2;
+                }
+
+                return new SistemaDecimal(valorDecimal.ToString());
+            }
+            else
+            {
+                return new SistemaDecimal(msgError);
+            }
+        }
+
+        internal override double ValorNumerico
+        {
+            get
+            {
+                if (!Valor.Contains(msgError) && !string.IsNullOrWhiteSpace(Valor))
+                {
+                    return BinarioADecimal().ValorNumerico;
+                }
+                return 0.0;
+            }
+        }
+
+        // Método para verificar si la cadena es un sistema binario válido
+        public static bool EsSistemaBinarioValido(string valor)
+        {
+            foreach (char digito in valor)
+            {
+                if (digito != '0' && digito != '1')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Conversión implícita de string a SistemaBinario
+        public static implicit operator SistemaBinario(string valor)
+        {
+            return new SistemaBinario(valor);
+        }
+    }
+
+
+}
+
